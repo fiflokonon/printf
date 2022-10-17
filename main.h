6754ba3a1fee656d625f7e4d35982747f35439ce
fiflokonon <fiflokonon@gmail.com>
2022-10-17 09:58:05 -0700

    firts

--- main.h
+++ main.h
@@ -1,2 +1,17 @@
+#ifndefine _MAIN_
+#define _MAIN_
+
 int _printf(const char *format, ...);
 int _putchar(char c);
+
+/**
+ * struct op - flag 
+ * @c: flag
+ * @f: function
+ */
+
+typedef struct op
+{
+	char *c;
+	int (*f)(va_list);
+} op_t;
