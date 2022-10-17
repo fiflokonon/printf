e319989206eead3cd9ee58444b08341073c56153
fiflokonon <fiflokonon@gmail.com>
2022-10-17 10:10:59 -0700

    weah

---
+++ main.h
@@ -0,0 +1,20 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * struct print - structure for printing various types
+ * @t: type to print
+ * @f: function to print
+ */
+typedef struct print
+{
+	char *t;
+	int (*f)(va_list);
+} print_t;
+
+int _printf(const char * format, ...);
+int _putchar(char c);
+
+#endif /* _MAIN_H_ */
